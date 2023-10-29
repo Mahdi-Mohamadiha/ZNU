d0a9cd87d6e30d137ee0f3afaf276e614a30fe5e
Mahdi-Mohamadiha <mahdi.mohamadiha.znu@gmail.com>
2023-10-29 19:07:18 +0330

    trigger for migration to create writer and comment

--- commentary/models.py
+++ commentary/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 # Create your models here.
+
+@receiver(post_migrate)
+def create_initial_data(sender, **kwargs):
+    if sender.name == 'commentary':
+        if Writer.objects.count() == 0:
+            writer = Writer.objects.create(first_name="NA", last_name="NA", sex="NA")
+            Comment.objects.create(writer=writer, title="NA", text="NA", reply=1)
+            print("Message: Initial data created successfully for Writer and Comment models.")
 
 class Writer(models.Model):
     first_name = models.CharField(max_length=127)
